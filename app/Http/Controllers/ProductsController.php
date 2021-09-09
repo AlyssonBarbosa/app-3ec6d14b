897f1fa3c81d920c24c87db3776fcc4d5a8749bc
Alysson Oliveira <alysson.o@willfly.live>
2021-09-08 22:19:44 -0300

    [feature] Atualizar quantidade de um produto [feature] Update quantity of a product
    - Adiciona endpoint para atualizar quantidade de um produto
    - Adiciona um arquivo de validação para atualizar a quantidade de um produto
    - Adiciona exceção para quantidade não permitida
    - Adiciona função para atualizar quantidade de produtos

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidValueException;
 use App\Http\Requests\Products\ProductStoreRequest;
+use App\Http\Requests\Products\ProductUpdateQuantity;
 use App\Models\Product;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -24,9 +28,35 @@ class ProductsController extends Controller
             DB::commit();
 
             return $this->response([], 'Produto criado com sucesso!', 201);
-        } catch (\Throwable $th) {            
+        } catch (\Throwable $th) {
             DB::rollback();
             $this->response($th, 'Não foi possível cadastrar o produto!', 400);
+        }
+    }
+
+    public function updateQuantityOnlySku(ProductUpdateQuantity $request, string $sku)
+    {
+        try {
+            $quantity = $request->quantity;
+
+            $product = Product::where('sku', $sku)->first();
+
+            if (!$product) {
+                throw new ModelNotFoundException("Product not found");
+            }
+            
+            if ($product->quantity + $quantity < 0) {
+                throw new InvalidValueException("Value is higher than allowed");
+            }
+
+            $product->update(['quantity' => $product->quantity + $request->quantity]);
+
+            return $this->response($product, "Quantidade atualizada com sucesso", 200);
+            
+        } catch (ModelNotFoundException | InvalidValueException $exception) {
+            return $this->response($exception->getMessage(), "Não foi possivel processar esta operação", 400);
+        } catch (\Throwable $th) {
+            return $this->response($th, "Erro no servidor", 500);
         }
     }
 }
